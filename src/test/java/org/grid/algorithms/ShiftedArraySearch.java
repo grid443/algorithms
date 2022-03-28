2d98f0c486613e17d28cfd819dab9d360adb80c3
Kirill Vasin <v.kirill.01@gmail.com>
2022-03-28 11:52:55 +0300

    Added parametrized tests

--- src/test/java/org/grid/algorithms/ShiftedArraySearch.java
+++ src/test/java/org/grid/algorithms/ShiftedArraySearch.java
@@ -1,8 +1,12 @@
 package org.grid.algorithms;
 
-import org.junit.jupiter.api.Test;
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 /**
  * A sorted array of distinct integers shiftArr is shifted to the left by an unknown offset
@@ -17,47 +21,16 @@ import static org.assertj.core.api.Assertions.assertThat;
  */
 public class ShiftedArraySearch {
 
-    @Test
-    void shifterArraySearchTest() {
-        assertThat(shiftedArrSearch(new int[]{3, 1, 2}, 1)).isEqualTo(1);
-        assertThat(shiftedArrSearch(new int[]{3, 1, 2}, 2)).isEqualTo(2);
-        assertThat(shiftedArrSearch(new int[]{3, 1, 2}, 3)).isEqualTo(0);
-        assertThat(shiftedArrSearch(new int[]{2, 3, 1}, 1)).isEqualTo(2);
-        assertThat(shiftedArrSearch(new int[]{2, 3, 1}, 2)).isEqualTo(0);
-        assertThat(shiftedArrSearch(new int[]{2, 3, 1}, 3)).isEqualTo(1);
-        assertThat(shiftedArrSearch(new int[]{1, 2, 3}, 1)).isEqualTo(0);
-        assertThat(shiftedArrSearch(new int[]{1, 2, 3}, 2)).isEqualTo(1);
-        assertThat(shiftedArrSearch(new int[]{1, 2, 3}, 3)).isEqualTo(2);
-        assertThat(shiftedArrSearch(new int[]{7, 3}, 7)).isEqualTo(0);
-        assertThat(shiftedArrSearch(new int[]{7, 3}, 3)).isEqualTo(1);
-        assertThat(shiftedArrSearch(new int[]{7, 3}, -1)).isEqualTo(-1);
-        assertThat(shiftedArrSearch(new int[]{7, 3}, 5)).isEqualTo(-1);
-        assertThat(shiftedArrSearch(new int[]{7, 3}, 8)).isEqualTo(-1);
-        assertThat(shiftedArrSearch(new int[]{7, 8, 9, 1, 2, 3}, 7)).isEqualTo(0);
-        assertThat(shiftedArrSearch(new int[]{7, 8, 9, 1, 2, 3}, 3)).isEqualTo(5);
-        assertThat(shiftedArrSearch(new int[]{7, 8, 9, 1, 2, 3}, 9)).isEqualTo(2);
-        assertThat(shiftedArrSearch(new int[]{7, 8, 9, 1, 2, 3}, 1)).isEqualTo(3);
-        assertThat(shiftedArrSearch(new int[]{7, 8, 9, 1, 2, 3}, 8)).isEqualTo(1);
-        assertThat(shiftedArrSearch(new int[]{7, 8, 9, 1, 2, 3}, 2)).isEqualTo(4);
-        assertThat(shiftedArrSearch(new int[]{7, 8, 9, 10, 2, 3}, 10)).isEqualTo(3);
-        assertThat(shiftedArrSearch(new int[]{7, 8, 9, 10, 2, 3}, 2)).isEqualTo(4);
-        assertThat(shiftedArrSearch(new int[]{7, 8, 0, 1, 2, 3}, 0)).isEqualTo(2);
-        assertThat(shiftedArrSearch(new int[]{7, 8, 0, 1, 2, 3}, 1)).isEqualTo(3);
-        assertThat(shiftedArrSearch(new int[]{5, 7, 78, 192, -34, 0, 1, 2}, -34)).isEqualTo(4);
-        assertThat(shiftedArrSearch(new int[]{5, 7, 78, 192, -34, 0, 1, 2}, 192)).isEqualTo(3);
-        assertThat(shiftedArrSearch(new int[]{5, 7, 78, 192, -34, 0, 1, 2}, -2)).isEqualTo(-1);
+    @MethodSource("arraySearchSource")
+    @ParameterizedTest(name = "Search for the key {1} in the array {0} should return {2}")
+    void shifterArraySearchTest(int[] shiftedArr, int key, int expectedResult) {
+        assertThat(shiftedArrSearch(shiftedArr, key)).isEqualTo(expectedResult);
     }
 
-    @Test
-    void shiftPointTest() {
-        assertThat(shiftPoint(new int[]{3, 1, 2})).isEqualTo(1);
-        assertThat(shiftPoint(new int[]{2, 3, 1})).isEqualTo(2);
-        assertThat(shiftPoint(new int[]{1, 2, 3})).isEqualTo(0);
-        assertThat(shiftPoint(new int[]{7, 3})).isEqualTo(1);
-        assertThat(shiftPoint(new int[]{7, 8, 9, 1, 2, 3})).isEqualTo(3);
-        assertThat(shiftPoint(new int[]{7, 8, 9, 10, 2, 3})).isEqualTo(4);
-        assertThat(shiftPoint(new int[]{7, 8, 0, 1, 2, 3})).isEqualTo(2);
-        assertThat(shiftPoint(new int[]{5, 7, 78, 192, -34, 0, 1, 2})).isEqualTo(4);
+    @MethodSource("shirtPointSource")
+    @ParameterizedTest(name = "Search for the shift point in the array {0} should return {1}")
+    void shiftPointTest(int[] shiftedArr, int expectedShiftPoint) {
+        assertThat(shiftPoint(shiftedArr)).isEqualTo(expectedShiftPoint);
     }
 
     private static int shiftedArrSearch(int[] shiftArr, int key) {
@@ -74,7 +47,7 @@ public class ShiftedArraySearch {
         int leftIndex = fromIndex;
         int rightIndex = toIndex - 1;
         while (leftIndex <= rightIndex) {
-            int middleIndex = (leftIndex + rightIndex) >>> 1;
+            int middleIndex = leftIndex + (rightIndex - leftIndex) / 2;
             int middleValue = arr[middleIndex];
             if (middleValue > key) {
                 rightIndex = middleIndex - 1;
@@ -113,5 +86,51 @@ public class ShiftedArraySearch {
         } else {
             return intValue + 1;
         }
+    }
+
+    private static Stream<Arguments> arraySearchSource() {
+        return Stream.of(
+                arguments(new int[]{3, 1, 2}, 1, 1),
+                arguments(new int[]{3, 1, 2}, 2, 2),
+                arguments(new int[]{3, 1, 2}, 3, 0),
+                arguments(new int[]{2, 3, 1}, 1, 2),
+                arguments(new int[]{2, 3, 1}, 2, 0),
+                arguments(new int[]{2, 3, 1}, 3, 1),
+                arguments(new int[]{1, 2, 3}, 1, 0),
+                arguments(new int[]{1, 2, 3}, 2, 1),
+                arguments(new int[]{1, 2, 3}, 3, 2),
+                arguments(new int[]{7, 3}, 7, 0),
+                arguments(new int[]{7, 3}, 3, 1),
+                arguments(new int[]{7, 3}, -1, -1),
+                arguments(new int[]{7, 3}, 5, -1),
+                arguments(new int[]{7, 3}, 8, -1),
+                arguments(new int[]{7, 8, 9, 1, 2, 3}, 7, 0),
+                arguments(new int[]{7, 8, 9, 1, 2, 3}, 3, 5),
+                arguments(new int[]{7, 8, 9, 1, 2, 3}, 9, 2),
+                arguments(new int[]{7, 8, 9, 1, 2, 3}, 1, 3),
+                arguments(new int[]{7, 8, 9, 1, 2, 3}, 8, 1),
+                arguments(new int[]{7, 8, 9, 1, 2, 3}, 2, 4),
+                arguments(new int[]{7, 8, 9, 10, 2, 3}, 10, 3),
+                arguments(new int[]{7, 8, 9, 10, 2, 3}, 2, 4),
+                arguments(new int[]{7, 8, 0, 1, 2, 3}, 0, 2),
+                arguments(new int[]{7, 8, 0, 1, 2, 3}, 1, 3),
+                arguments(new int[]{5, 7, 78, 192, -34, 0, 1, 2}, -34, 4),
+                arguments(new int[]{5, 7, 78, 192, -34, 0, 1, 2}, 192, 3),
+                arguments(new int[]{5, 7, 78, 192, -34, 0, 1, 2}, -2, -1)
+        );
+    }
+
+    private static Stream<Arguments> shirtPointSource() {
+        return Stream.of(
+                arguments(new int[]{3, 1, 2}, 1, 1),
+                arguments(new int[]{3, 1, 2}, 1),
+                arguments(new int[]{2, 3, 1}, 2),
+                arguments(new int[]{1, 2, 3}, 0),
+                arguments(new int[]{7, 3}, 1),
+                arguments(new int[]{7, 8, 9, 1, 2, 3}, 3),
+                arguments(new int[]{7, 8, 9, 10, 2, 3}, 4),
+                arguments(new int[]{7, 8, 0, 1, 2, 3}, 2),
+                arguments(new int[]{5, 7, 78, 192, -34, 0, 1, 2}, 4)
+        );
     }
 }
